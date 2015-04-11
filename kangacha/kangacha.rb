ea0c1986725c99669e2c524108a2293641406334
sekai013 <sekai013@gmail.com>
2015-04-11 10:51:49 +0900

    #31 wakaran

--- kangacha/kangacha.rb
+++ kangacha/kangacha.rb
@@ -1,21 +1,30 @@
 require 'uri'
 require 'net/http'
+require 'execjs'
 
 uri        = URI.parse "http://ctfq.sweetduet.info:10080/~q31/kangacha.php"
-known_ship = "1"
+known_ship = "1;"
 known_sign = "24b7447578c89ea8f5f8854d60e253f23bb5b8856d8a135c19af423db354ac60a1a4c932cecd800a0550211e8cc6e28e73e1ac93e7b9c786adc24702e48701c5"
-add_data   = "%2C10"
+add_data   = "ship=10"
 trials     = 50
 
 http = Net::HTTP.new uri.host, uri.port
 http.start {
 	failed = http.get(uri.path).body
 
-	(5..trials).each do |i|
-		hashdump = `hashpump -s #{known_sign} -d #{known_ship} -a #{add_data} -k #{i}`
-		sign, data = hashdump.split "\n"
+=begin
+	known_header = {
+		'Cookie' => "ship=#{known_ship}; signature=#{known_sign}"
+	}
+	puts http.get(uri.path, known_header).body
+=end
 
-		# data をいい感じに処理
+	(5..trials).each do |i|
+		hashpump = `hashpump -s #{known_sign} -d "#{known_ship}" -a "#{add_data}" -k #{i}`
+		sign, data = hashpump.split "\n"
+
+		# この data さえうまく処理できればいけそうたぶん
+		data = ExecJS.eval %(encodeURIComponent("#{data}"))
 
 		header = {
 			'Cookie' => "ship=#{data}; signature=#{sign}"
